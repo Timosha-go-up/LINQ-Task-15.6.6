b2c1453bd3da5a9a2eeacc447058a1c6482f6214
Roman Ochagow <ohagow.r@gmail.com>
2025-09-22 18:41:37 +0500

    update task15.6.6

---
+++ Task-15-6-6/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_15_6_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+             {
+                new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"} },
+                new Classroom { Students = {"Anna", "Viktor", "Vladimir"} },
+                new Classroom { Students = {"Bulat", "Alex", "Galina"} }
+            };
+            var allStudents = GetAllStudents(classes);
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        // Метод для получения всех учеников
+        static string[] GetAllStudents(Classroom[] classes)
+        {           
+            return classes .SelectMany(classroom => classroom.Students).ToArray();                                
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
